fe64bbd355cd70117ab20d6c5ce4ff0b8893c576
Ivannikov Roman <shark71@inbox.ru>
2022-11-26 21:34:09 +0300

    Решили четвертую задачу

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,22 @@
+﻿// 4. Напишите программу,  которая выводит случайное трехзначное число и удаляет вторую цифру этого числа, не использовать строки для рассчета.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+
+System.Console.WriteLine();
+
+int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+
+int NumSecDig()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+
+NumSecDig();
+
+System.Console.WriteLine($"{Number} -> {NewNumber}");
+
